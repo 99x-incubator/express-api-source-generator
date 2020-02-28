2e1746834f85ef5a30e04692336940984e90060f
Anjula Karunarathne <anjulakarunarathne@gmail.com>
2020-02-29 00:03:26 +0530

    Add intro

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("Hello, World!")
+const chalk = require('chalk');
+var figlet = require('figlet');
+const log = console.log;
+
+figlet('express-starter', function(err, data) {
+    if (err) {
+        log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    log(data)
+    log(chalk.blue("\nWelcome to the interacive Express bootstrapper!"))
+});

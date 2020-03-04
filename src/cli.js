294f64869472ffaa5b27797e331cf453f28eb482
Anjula Karunarathne <anjulakarunarathne@gmail.com>
2020-03-04 23:39:26 +0530

    Update CLI interface to accept name as an argument

--- src/cli.js
+++ src/cli.js
@@ -5,11 +5,12 @@ import { createProject } from './main'
 function parseArgumentsIntoOptions(rawArgs) {
     const args = arg(
         {
-            '--git': Boolean,
             '--yes': Boolean,
+            '--template': String,
+            '--git': Boolean,
             '--install': Boolean,
-            '-g': '--git',
             '-y': '--yes',
+            '-g': '--git',
             '-i': '--install',
         },
         {
@@ -17,51 +18,73 @@ function parseArgumentsIntoOptions(rawArgs) {
         }
     );
     return {
-        // either default values or provided arguments
+        name: args._[0],
+        template: args['--template'] || 'Default',
         skipPrompts: args['--yes'] || false,
         git: args['--git'] || false,
-        template: args._[0],
         runInstall: args['--install'] || false,
     };
 }
 
 async function promptForMissingOptions(options) {
-    const defaultTemplate = 'JavaScript';
+    const defaultTemplate = 'Default';
+    const questions = [];
+
+    if (!options.name) {
+        questions.push({
+            type: 'input',
+            name: 'name',
+            message: 'Please type in your project name'
+        });
+    }
+
     if (options.skipPrompts) {
+        const answers = await inquirer.prompt(questions);
         return {
             ...options,
+            name: options.name || answers.name,
             template: options.template || defaultTemplate,
         };
     }
+    else {
+        if (!options.template) {
+            questions.push({
+                type: 'list',
+                name: 'template',
+                message: 'Please choose which project template to use',
+                choices: ['Default', 'Basic'],
+                default: defaultTemplate,
+            });
+        }
 
-    const questions = [];
-    if (!options.template) {
-        questions.push({
-            type: 'list',
-            name: 'template',
-            message: 'Please choose which project template to use',
-            choices: ['JavaScript', 'TypeScript'],
-            default: defaultTemplate,
-        });
+        if (!options.git) {
+            questions.push({
+                type: 'confirm',
+                name: 'git',
+                message: 'Initialize a git repository?',
+                default: false,
+            });
+        }
+
+        if (!options.install) {
+            questions.push({
+                type: 'confirm',
+                name: 'install',
+                message: 'Install all dependencies?',
+                default: false,
+            });
+        }
+
+        const answers = await inquirer.prompt(questions);
+        return {
+            ...options,
+            name: options.name || answers.name,
+            template: options.template || answers.template,
+            git: options.git || answers.git,
+            install: options.install || answers.install,
+        };
     }
-
-    if (!options.git) {
-        questions.push({
-            type: 'confirm',
-            name: 'git',
-            message: 'Initialize a git repository?',
-            default: false,
-        });
-    }
-
-    const answers = await inquirer.prompt(questions);
-    return {
-        ...options,
-        template: options.template || answers.template,
-        git: options.git || answers.git,
-    };
 }
-
 
 export async function cli(args) {
     let options = parseArgumentsIntoOptions(args);

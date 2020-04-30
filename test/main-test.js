269fb863daaf8b26945cf4084f8e68a0d88dc48b
Anjula Karunarathne <anjulakarunarathne@gmail.com>
2020-04-30 23:15:40 +0530

    Write unit-tests for validating project names

---
+++ test/main-test.js
@@ -0,0 +1,22 @@
+require = require('esm')(module /*, options*/);
+
+const assert = require('assert');
+const main = require('../src/main');
+
+describe('Validate project names', function () {
+  it('should return true if provided a valid project name', async function () {
+    return main.validateProjectName('@anjulalk/hello-world').then(result => {
+      assert.equal(result, true)
+    })
+  });
+
+  it('should throw error if provided an invalid project name', async function () {
+    return main.validateProjectName('hello\\world').then(result => {
+      throw new Error('was not supposed to succeed');
+    }).catch(err => {
+      return assert(err, 'name can only contain URL-friendly characters');
+    }
+    )
+  });
+
+});
